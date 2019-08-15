53b1fc67538ec006459bf64c2aa45a479bf22985
Ben Brott <benbrott@Bens-MacBook-Pro.local>
2019-08-14 23:15:57 -0400

    Added dark mode theme and toggle

--- src/App/App.js
+++ src/App/App.js
@@ -1,59 +1,106 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { HashRouter, Route, Link } from "react-router-dom";
+import classNames from 'classnames';
 import styles from './App.module.css';
 import Home from './Home/Home'
 import Music from './Music/Music'
-import headphones from './headphones.svg';
-import logoLight from './logoLight.svg';
+import headphonesLight from './assets/headphonesLight.svg';
+import headphonesDark from './assets/headphonesDark.svg';
+import logoLight from './assets/logoLight.svg';
+import logoDark from './assets/logoDark.svg';
+import sunLight from './assets/sunLight.svg';
+import moonDark from './assets/moonDark.svg';
+import { KEYS } from '../utils/events';
 
 const PATH_HOME = '/';
 const PATH_MUSIC = '/music';
 
-const NAV_ICONS = [
-  {
-    path: PATH_HOME,
-    icon: logoLight,
-    alt: 'home'
-  },
-  {
-    path: PATH_MUSIC,
-    icon: headphones,
-    alt: 'music'
-  }
-];
+const THEME = 'THEME';
+const LIGHT = 'LIGHT';
+const DARK = 'DARK';
+const LIGHT_BACKGROUND = '#bbb';
+const DARK_BACKGROUND = '#333';
 
 class App extends React.Component {
-  onNavIconClick = (alt) => {
+  constructor(props) {
+    super(props);
+    this.state = { isDark: localStorage.getItem(THEME) === DARK };
+  }
+
+  componentDidMount() {
+    document.body.style.backgroundColor = this.state.isDark ? DARK_BACKGROUND : LIGHT_BACKGROUND;
+  }
+
+  componentWillUnmount() {
+    document.body.style.backgroundColor = null;
+  }
+
+  onNavIconClick = alt => {
     ReactDom.findDOMNode(this.refs[alt]).blur();
   }
 
-  renderNavIcons = () => {
-    return NAV_ICONS.map(({path, icon, alt}, index) => {
+  renderNavIcons = isDark => {
+    const containerClasses = classNames([styles.iconContainer, isDark && styles.dark]);
+    return [
+      {
+        path: PATH_HOME,
+        icon: isDark ? logoLight : logoDark,
+        alt: 'home'
+      },
+      {
+        path: PATH_MUSIC,
+        icon: isDark ? headphonesLight : headphonesDark,
+        alt: 'music'
+      }
+    ].map(({path, icon, alt}, index) => {
+      const onClick = () => this.onNavIconClick(alt);
+      const key = `navIcon_${index}`;
       return (
-        <Link
-          to={path}
-          className={styles.iconContainer}
-          onClick={() => this.onNavIconClick(alt)}
-          ref={alt}
-          key={`navIcon_${index}`}
-        >
+        <Link to={path} className={containerClasses} onClick={onClick} ref={alt} key={key}>
           <img src={icon} className={styles.icon} alt={alt} />
         </Link>
       );
     });
   }
 
-  renderNavBar = () => {
+  toggleTheme = () => {
+    const toggleDark = !this.state.isDark;
+    localStorage.setItem(THEME, toggleDark ? DARK : LIGHT);
+    document.body.style.backgroundColor = toggleDark ? DARK_BACKGROUND : LIGHT_BACKGROUND;
+    this.setState({ isDark: toggleDark });
+  }
+
+  onThemeIconKeyPress = event => {
+    if (event.key === KEYS.ENTER) {
+      this.toggleTheme();
+    }
+  }
+
+  renderThemeIcon = isDark => {
+    const containerClasses = classNames([styles.iconContainer, isDark && styles.dark]);
+    const src = isDark ? sunLight : moonDark;
+    const alt = isDark ? 'sun' : 'moon';
     return (
-      <div className={styles.navBar}>
-        {this.renderNavIcons()}
+      <div className={containerClasses} onClick={this.toggleTheme} onKeyPress={this.onThemeIconKeyPress} tabIndex={0}>
+        <img src={src} className={styles.icon} alt={alt} />
       </div>
     );
   }
 
-  homeComponent = () => <Home />
-  musicComponent = () => <Music />
+  renderNavBar = () => {
+    const isDark = this.state.isDark;
+    const containerClasses = classNames([styles.navBar, isDark && styles.dark]);
+    return (
+      <div className={containerClasses}>
+        {this.renderNavIcons(isDark)}
+        {this.renderThemeIcon(isDark)}
+      </div>
+    );
+  }
+
+  homeComponent = () => <Home isDark={this.state.isDark}/>
+  musicComponent = () => <Music isDark={this.state.isDark}/>
 
   render() {
     return (
--- src/App/Home/Home.js
+++ src/App/Home/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Home.module.css';
 import logoNameFlatLight from './logoNameFlatLight.svg';
+import logoNameFlatDark from './logoNameFlatDark.svg';
 
 class Home extends React.Component {
+  static propTypes = {
+    isDark: PropTypes.bool
+  }
+
+  static defaultProps = {
+    isDark: false
+  }
+
   render() {
     return (
       <div className={styles.container}>
-        <img src={logoNameFlatLight} className={styles.banner} alt="" />
+        <img src={this.props.isDark ? logoNameFlatLight : logoNameFlatDark} className={styles.banner} alt="" />
       </div>
     );
   }
