4df72cc0ff7adaf821b00c13ef7297b2c9bc2ca9
Ben Brott <benbrott@Bens-MacBook-Pro.local>
2019-08-15 07:51:35 -0400

    Added window resize listener to update name banner

--- src/App/Home/Home.js
+++ src/App/Home/Home.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import throttle from 'lodash.throttle';
 import styles from './Home.module.css';
+import logoNameLight from './logoNameLight.svg';
+import logoNameDark from './logoNameDark.svg';
 import logoNameFlatLight from './logoNameFlatLight.svg';
 import logoNameFlatDark from './logoNameFlatDark.svg';
 
 class Home extends React.Component {
-  static propTypes = {
-    isDark: PropTypes.bool
+  static propTypes = { isDark: PropTypes.bool }
+  static defaultProps = { isDark: false }
+
+  constructor(props) {
+    super(props);
+    this.state = { isNarrow: this.isNarrowScreen() };
   }
 
-  static defaultProps = {
-    isDark: false
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize); 
+  }
+
+  isNarrowScreen = () => window.matchMedia('(max-width: 56em)').matches
+
+  handleResize = throttle(() => {
+    const isNarrow = this.isNarrowScreen();
+    if (this.state.isNarrow !== isNarrow) {
+      this.setState({ isNarrow });
+    }
+  }, 100)
+
+  getBannerSrc = () => {
+    const isDark = this.props.isDark;
+    if (this.state.isNarrow) {
+      return isDark ? logoNameLight : logoNameDark;
+    }
+    return isDark ? logoNameFlatLight : logoNameFlatDark;
   }
 
   render() {
     return (
       <div className={styles.container}>
-        <img src={this.props.isDark ? logoNameFlatLight : logoNameFlatDark} className={styles.banner} alt="" />
+        <img src={this.getBannerSrc()} className={styles.banner} alt="" />
       </div>
     );
   }
