cc8ba86f278f77b230577a8ff8f4cee12dd05a04
Ben Brott <benbrott@Bens-MacBook-Pro.local>
2019-08-15 22:23:39 -0400

    Finished adding album data

--- src/App/Music/data.js
+++ src/App/Music/data.js
@@ -1,9 +1,16 @@
 export const DATA = [
   {
+    artist: 'Adele',
+    title: '21',
+    src: 'https://i.scdn.co/image/26007fcd7781ea9222b23ab3654ba86f60dd6e18',
+    backgroundColor: '#1e1e1e',
+    color: '#bbbbbb'
+  },
+  {
     artist: 'Anderson .Paak',
     title: 'Malibu',
     src: 'https://i.scdn.co/image/8f1948fa598aae4a5e804c7b144b77f3d4bbd181',
-    backgroundColor: '#942a14',
+    backgroundColor: '#cc2d0b',
     color: '#8ebf8e'
   },
   {
@@ -14,11 +21,25 @@ export const DATA = [
     color: '#3c3838'
   },
   {
+    artist: 'Andrew Lloyd Webber',
+    title: 'The Phantom of the Opera',
+    src: 'https://i.scdn.co/image/33f38bda084b88f6e2f02630c73022eb103e4a5f',
+    backgroundColor: '#000000',
+    color: '#ffffff'
+  },
+  {
     artist: 'The Arcs',
     title: 'Yours, Dreamily,',
     src: 'https://i.scdn.co/image/ffed1531cb2e195d0989dcc1abcfec154ac98de2',
     backgroundColor: '#da992a',
     color: '#231921'
+  },
+  {
+    artist: 'Arctic Monkeys',
+    title: 'AM',
+    src: 'https://i.scdn.co/image/f513ae2d610d9255913795d4bc28ca33c827b232',
+    backgroundColor: '#000000',
+    color: '#ffffff'
   },
   {
     artist: 'AWOLNATION',
@@ -29,7 +50,7 @@ export const DATA = [
   },
   {
     artist: 'Bas',
-    title: 'Too High To Riot',
+    title: 'Too High to Riot',
     src: 'https://i.scdn.co/image/f8646bee72a7768105189d3c345dd38f12bcb103',
     backgroundColor: '#ada3a1',
     color: '#3d3d3d'
@@ -85,6 +106,13 @@ export const DATA = [
   },
   {
     artist: 'Cold War Kids',
+    title: 'Hold My Home',
+    src: 'https://i.scdn.co/image/4517816fb40750bc54014d239ca64ac409c273da',
+    backgroundColor: '#ffffff',
+    color: '#000000'
+  },
+  {
+    artist: 'Cold War Kids',
     title: 'Robbers & Cowards',
     src: 'https://i.scdn.co/image/18a6f73dd2af81c56e4549aa1eb7b6a880737fdd',
     backgroundColor: '#29301e',
@@ -96,6 +124,13 @@ export const DATA = [
     src: 'https://i.scdn.co/image/a7d3fbe053e83e5f7a61204cd3c4929b741b4a0a',
     backgroundColor: '#a4972d',
     color: '#000000'
+  },
+  {
+    artist: 'Declan McKenna',
+    title: 'What Do You Think About the Car?',
+    src: 'https://i.scdn.co/image/dc9d2b0cf38e51fb3f7e58ca372bdda2b8b8daca',
+    backgroundColor: '#dddddd',
+    color: '#352524'
   },
   {
     artist: 'The Districts',
@@ -112,11 +147,32 @@ export const DATA = [
     color: '#2f3976'
   },
   {
+    artist: 'The Districts',
+    title: 'Telephone',
+    src: 'https://i.scdn.co/image/5e4f6883e5efe55599d3925cf2f05231a1337eee',
+    backgroundColor: '#9a8a5f',
+    color: '#403328'
+  },
+  {
+    artist: 'Donnie Trumpet & The Social Experiment',
+    title: 'Surf',
+    src: 'https://i.scdn.co/image/a32c2510cbe897256c4f4a8204eba79f895f24c5',
+    backgroundColor: '#ffffff',
+    color: '#666666'
+  },
+  {
     artist: 'Drake',
     title: 'Take Care',
     src: 'https://i.scdn.co/image/fc52fd54ad72190904dd0e82055b32c14dee0f04',
     backgroundColor: '#bc8548',
     color: '#000000'
+  },
+  {
+    artist: 'Drake',
+    title: 'Thank Me Later',
+    src: 'https://i.scdn.co/image/f7565507d22e18a50d8e615852c5a2f928ddb556',
+    backgroundColor: '#ffffff',
+    color: '#e72a2e'
   },
   {
     artist: 'Electric Guest',
@@ -148,6 +204,13 @@ export const DATA = [
   },
   {
     artist: 'Foster the People',
+    title: 'Supermodel',
+    src: 'https://i.scdn.co/image/ad1ae891423adf3842ad505d10b72c148caeccd5',
+    backgroundColor: '#2d7581',
+    color: '#ecb035'
+  },
+  {
+    artist: 'Foster the People',
     title: 'Torches',
     src: 'https://i.scdn.co/image/fb4998c955d8eba049ffbfc7ab8523a4a4a1f7e9',
     backgroundColor: '#ffffff',
@@ -169,7 +232,7 @@ export const DATA = [
   },
   {
     artist: 'Glass Animals',
-    title: 'How To Be A Human Being',
+    title: 'How to Be a Human Being',
     src: 'https://i.scdn.co/image/1cd723595cfd4dd9375cfdf980b1b9b0134913eb',
     backgroundColor: '#ce722b',
     color: '#fbeccc'
@@ -182,11 +245,25 @@ export const DATA = [
     color: '#e69b26'
   },
   {
+    artist: 'GOOD Music',
+    title: 'Cruel Summer',
+    src: 'https://i.scdn.co/image/a905e57a1dcc157cd8c5ad77c9f264c45268c664',
+    backgroundColor: '#efefef',
+    color: '#626262'
+  },
+  {
     artist: 'Highly Suspect',
     title: 'The Boy Who Died Wolf',
     src: 'https://i.scdn.co/image/c2afd4ee2dc6ae704733654c20ca772abe0e590f',
     backgroundColor: '#2d6981',
     color: '#1b140a'
+  },
+  {
+    artist: 'J. Cole',
+    title: '4 Your Eyez Only',
+    src: 'https://i.scdn.co/image/51ae308118bec4be617c5cfd2061d7ace6ca499b',
+    backgroundColor: '#252525',
+    color: '#e2e2e2'
   },
   {
     artist: 'J. Cole',
@@ -225,10 +302,17 @@ export const DATA = [
   },
   {
     artist: 'James Vincent McMorrow',
-    title: 'We Don\'t Eat',
+    title: 'We Don\'t Eat EP',
     src: 'https://i.scdn.co/image/a955d987b82e25135677ebc9ae91c56e8e91a1cf',
     backgroundColor: '#a2b2b2',
     color: '#2d383a'
+  },
+  {
+    artist: 'JAY Z',
+    title: 'The Blueprint 3',
+    src: 'https://i.scdn.co/image/b8a3b08a5a6f6824d18b80c2034fb85e51fdb9c7',
+    backgroundColor: '#9c222a',
+    color: '#e7e7e7'
   },
   {
     artist: 'JAY Z, Kanye West',
@@ -243,6 +327,13 @@ export const DATA = [
     src: 'https://i.scdn.co/image/a80ff343af9ab34ca312b91a01483fc846591a33',
     backgroundColor: '#f09b0c',
     color: '#101010'
+  },
+  {
+    artist: 'Joey Bada$$',
+    title: 'ALL-AMERIKKKAN BADA$$',
+    src: 'https://pl.scdn.co/images/pl/default/64e2a2520253e7aa36cf015d090e4bd935382003',
+    backgroundColor: '#96c3d6',
+    color: '#c2594b'
   },
   {
     artist: 'Joey Bada$$',
@@ -322,6 +413,125 @@ export const DATA = [
     color: '#0967a6'
   },
   {
+    artist: 'The Killers',
+    title: 'Sam\'s Town',
+    src: 'https://i.scdn.co/image/3d7b7545a896adc90ba3926be09d4cd3858c7bcb',
+    backgroundColor: '#e3e7ea',
+    color: '#050505'
+  },
+  {
+    artist: 'Logic',
+    title: 'The Incredible True Story',
+    src: 'https://i.scdn.co/image/6d25bd982779e26081c55c5760a94fef529cd855',
+    backgroundColor: '#1a4582',
+    color: '#e8d88d'
+  },
+  {
+    artist: 'Logic',
+    title: 'Under Pressure',
+    src: 'https://i.scdn.co/image/eb3752f897c83b42a4c66512466b2d0652e54aa3',
+    backgroundColor: '#c1bbac',
+    color: '#1b3136'
+  },
+  {
+    artist: 'Lupe Fiasco',
+    title: 'The Cool',
+    src: 'https://i.scdn.co/image/80d4ce3f70072ed03b72ba4b17d2a72d2d0cfe31',
+    backgroundColor: '#d6ca49',
+    color: '#000000'
+  },
+  {
+    artist: 'Lupe Fiasco',
+    title: 'Food & Liquor',
+    src: 'https://i.scdn.co/image/520bc52bb0ebfe7554bafc55992071efa2e96fec',
+    backgroundColor: '#b7b3d7',
+    color: '#1e1e1e'
+  },
+  {
+    artist: 'Mac Miller',
+    title: 'Divine Feminine',
+    src: 'https://i.scdn.co/image/ba53abe8e80c4127e5bc8bec23af81e5a6225093',
+    backgroundColor: '#d9c0bb',
+    color: '#1e1e1e'
+  },
+  {
+    artist: 'Mosie',
+    title: 'Tangerine',
+    src: 'https://i.scdn.co/image/6acd293555254a74c956daa52f91ad92e23d121c',
+    backgroundColor: '#eb7b04',
+    color: '#76092a'
+  },
+  {
+    artist: 'Noname',
+    title: 'Room 25',
+    src: 'https://i.scdn.co/image/2de83faa6cf283cd56743451ff3f4d2e16342472',
+    backgroundColor: '#ffffff',
+    color: '#000000'
+  },
+  {
+    artist: 'NxWorries',
+    title: 'Yes Lawd!',
+    src: 'https://i.scdn.co/image/d9517e61311dc7b37a8f7447dfa1d9ecb93c6739',
+    backgroundColor: '#ebe5d7',
+    color: '#000000'
+  },
+  {
+    artist: 'Of Monsters and Men',
+    title: 'My Head Is an Animal',
+    src: 'https://i.scdn.co/image/af5597ca69007b39e6be0d424f6a169e92cbaff0',
+    backgroundColor: '#cedce0',
+    color: '#564d40'
+  },
+  {
+    artist: 'OneRepublic',
+    title: 'Native',
+    src: 'https://i.scdn.co/image/18289913f4954cfffc42158f84e9e3c955ae1923',
+    backgroundColor: '#cc2410',
+    color: '#ffffff'
+  },
+  {
+    artist: 'Pell',
+    title: 'Floating While Dreaming',
+    src: 'https://i.scdn.co/image/4cf84cd8d42c1ead2e7d05735941dfbd3edf1f54',
+    backgroundColor: '#eed8a9',
+    color: '#352935'
+  },
+  {
+    artist: 'Portugal. The Man',
+    title: 'Evil Friends',
+    src: 'https://i.scdn.co/image/83bfab824fbd6f7ac96ad44dc4d4be78a1a7b0f3',
+    backgroundColor: '#c8a74a',
+    color: '#000000'
+  },
+  {
+    artist: 'Pusha T',
+    title: 'Daytona',
+    src: 'https://i.scdn.co/image/a159c06b308a6fb14ef85e962ade81c30fd2a44a',
+    backgroundColor: '#bcb0a0',
+    color: '#1d2024'
+  },
+  {
+    artist: 'Pusha T',
+    title: 'King Push - Darkest Before Dawn: The Prelude',
+    src: 'https://i.scdn.co/image/6318de5a856a793163e217f23e6168450c57211d',
+    backgroundColor: '#474747',
+    color: '#e9e9e9'
+  },
+  {
+    artist: 'Pusha T',
+    title: 'My Name Is My Name',
+    src: 'https://i.scdn.co/image/789c6a88ba54d143af7877fc30aad3d831393f89',
+    backgroundColor: '#ffffff',
+    color: '#000000'
+  },
+  {
+    artist: 'Raury',
+    title: 'All We Need',
+    src: 'https://i.scdn.co/image/a12a3b925d49fbe32dbca90a19898b879a5457d9',
+    backgroundColor: '#c7835c',
+    color: '#000000'
+  },
+  {
     artist: 'RÜFÜS DU SOL',
     title: 'Bloom',
     src: 'https://i.scdn.co/image/ea3325205abf3bab41556091e1bbec267e8656c6',
@@ -329,10 +539,164 @@ export const DATA = [
     color: '#ffffff'
   },
   {
-    artist: 'Wicked Cast',
+    artist: 'Saint Motel',
+    title: 'saintmotelevision',
+    src: 'https://i.scdn.co/image/0f01ae79be1a6fd3e72b02ef8e6cc68d000b0af2',
+    backgroundColor: '#a64d3b',
+    color: '#f1c08b'
+  },
+  {
+    artist: 'Saint Motel',
+    title: 'Voyeur',
+    src: 'https://i.scdn.co/image/43be68e9c1d86451e55cc2e70c782d568591269d',
+    backgroundColor: '#417360',
+    color: '#d5c796'
+  },
+  {
+    artist: 'Shakey Graves',
+    title: 'And the War Came',
+    src: 'https://i.scdn.co/image/f546d600a395b07f0079a7cc3e5b4da9c09e2319',
+    backgroundColor: '#dddddd',
+    color: '#101710'
+  },
+  {
+    artist: 'The Shins',
+    title: 'Wincing the Night Away',
+    src: 'https://i.scdn.co/image/28319e7683072624fe6bd251599ca18937d26fd8',
+    backgroundColor: '#edddc4',
+    color: '#131109'
+  },
+  {
+    artist: 'Stephen Schwartz',
     title: 'Wicked',
     src: 'https://i.scdn.co/image/e4d0828daf13bb64da28976a032a3899369da6c9',
     backgroundColor: '#7cb33a',
     color: '#000000'
+  },
+  {
+    artist: 'Still Woozy',
+    title: 'Lately EP',
+    src: 'https://i.scdn.co/image/e43306ad08e48944e3fe92e5b5c72d84fd820bc3',
+    backgroundColor: '#1cc2f9',
+    color: '#ecf900'
+  },
+  {
+    artist: 'The Strokes',
+    title: 'Is This It',
+    src: 'https://i.scdn.co/image/36ad5132eee494bb519cd43e70fafe427cab95de',
+    backgroundColor: '#f1c342',
+    color: '#227c83'
+  },
+  {
+    artist: 'Sufjan Stevens',
+    title: 'Carrie & Lowell',
+    src: 'https://i.scdn.co/image/a0779f588d1f1f50921a083f5a1fc4217f0ddf7d',
+    backgroundColor: '#495d64',
+    color: '#ffffff'
+  },
+  {
+    artist: 'Sylvan Esso',
+    title: 'Sylvan Esso',
+    src: 'https://i.scdn.co/image/3fe606ee0c0bd62492628687b83b63c4d0d6bcf6',
+    backgroundColor: '#000000',
+    color: '#f2d16a'
+  },
+  {
+    artist: 'Tame Impala',
+    title: 'Currents',
+    src: 'https://i.scdn.co/image/fed3593129c6254d7620b58f5af2754dff0c36a8',
+    backgroundColor: '#775a79',
+    color: '#ebbb50'
+  },
+  {
+    artist: 'Tokyo Police Club',
+    title: 'Champ',
+    src: 'https://i.scdn.co/image/ff7a53508a8587af1148f8bff665f930b222d618',
+    backgroundColor: '#ffffff',
+    color: '#252525'
+  },
+  {
+    artist: 'Travelers',
+    title: 'Sleeptalking',
+    src: 'https://i.scdn.co/image/e0daa9bb73e8a39cb83bf1d789a01c758c9e1ed0',
+    backgroundColor: '#1c1c1c',
+    color: '#c3c7a9'
+  },
+  {
+    artist: 'Vince Staples',
+    title: 'Summertime \'06',
+    src: 'https://i.scdn.co/image/17576d8352989483a3067003e929ca400a74d3e6',
+    backgroundColor: '#fbf4d7',
+    color: '#000000'
+  },
+  {
+    artist: 'Wale',
+    title: 'The Album About Nothing',
+    src: 'https://i.scdn.co/image/6893694e2a6842d4cfef5dbc5a88eee71f12a5d4',
+    backgroundColor: '#f7033f',
+    color: '#12fadb'
+  },
+  {
+    artist: 'Wale',
+    title: 'Attention Deficit',
+    src: 'https://i.scdn.co/image/f5fd0927d8b41a90d7d9feafae3060619a7d9523',
+    backgroundColor: '#4c8cc9',
+    color: '#e1eff0'
+  },
+  {
+    artist: 'WALK THE MOON',
+    title: 'Walk the Moon',
+    src: 'https://i.scdn.co/image/c9544bf73e1fb770fb39ffa54c30598a41f80a79',
+    backgroundColor: '#4b58a1',
+    color: '#ffffff'
+  },
+  {
+    artist: 'Wolfmother',
+    title: 'Wolfmother',
+    src: 'https://i.scdn.co/image/aa0385072a34fb7b43bfd9454137f00012ec2f8e',
+    backgroundColor: '#443222',
+    color: '#ccb892'
+  },
+  {
+    artist: 'The xx',
+    title: 'xx',
+    src: 'https://i.scdn.co/image/765dd18a89c2dbbb8d548dfb6387001eb362d25c',
+    backgroundColor: '#000000',
+    color: '#ffffff'
+  },
+  {
+    artist: 'YBN Cordae',
+    title: 'The Lost Boy',
+    src: 'https://i.scdn.co/image/1036782a6cd826b580b77efc0e871b2cf9dfec73',
+    backgroundColor: '#603700',
+    color: '#bdac48'
+  },
+  {
+    artist: 'Young the Giant',
+    title: 'Home of the Strange',
+    src: 'https://www.youngthegiant.com/sites/g/files/g2000007966/f/styles/music_600_600/public/201608/YTG_HOTS_AlbumArt_1000_1.jpg?itok=MotHU_rS',
+    backgroundColor: '#f0ece9',
+    color: '#353340'
+  },
+  {
+    artist: 'Young the Giant',
+    title: 'Mind Over Matter',
+    src: 'https://i.scdn.co/image/91a92892cfeefad2159ddf831a0d868157a487c4',
+    backgroundColor: '#d3d3d3',
+    color: '#404040'
+  },
+  {
+    artist: 'Young the Giant',
+    title: 'Mirror Master',
+    src: 'https://www.youngthegiant.com/sites/g/files/g2000007966/f/styles/music_600_600/public/201810/YTG_MirrorMaster_Cover_HR_final.jpg?itok=h2cudpVA',
+    backgroundColor: '#9ce1f4',
+    color: '#1a1a1a'
+  },
+  {
+    artist: 'Young the Giant',
+    title: 'Young the Giant',
+    src: 'https://i.scdn.co/image/443c2f393d74bbe325d50b8b13b59bf2f74d3fde',
+    backgroundColor: '#d5ddce',
+    color: '#354f43'
   }
 ];
