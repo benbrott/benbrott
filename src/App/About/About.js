1aa1cf685d1347438a2e9484afb37476dd7a99c3
Ben Brott <benbrott@Bens-MacBook-Pro.local>
2019-09-05 20:50:01 -0400

    Updated About page text

--- src/App/About/About.js
+++ src/App/About/About.js
@@ -8,6 +8,13 @@ import logoNameDark from 'assets/logoNameDark.svg';
 import logoNameFlatLight from 'assets/logoNameFlatLight.svg';
 import logoNameFlatDark from 'assets/logoNameFlatDark.svg';
 
+const TEXT_MAPS = [
+  { label: 'WHO', info: 'Ben Brott, software engineer at Appian' },
+  { label: 'WHAT', info: 'A site inspired by talks at An Event Apart DC' },
+  { label: 'WHY', info: 'To learn CSS, explore SVG, and design eye-catching, responsive UIs' },
+  { label: 'HOW', info: 'React, Sass, GitHub Pages' }
+];
+
 class About extends React.PureComponent {
   static propTypes = { isDark: PropTypes.bool }
   static defaultProps = { isDark: false }
@@ -40,30 +47,31 @@ class About extends React.PureComponent {
     return isDark ? logoNameFlatLight : logoNameFlatDark;
   }
 
-  renderText = () => {
-    const isDark = this.props.isDark;
-    const textStyles = classNames([
-      styles.text,
-      isDark ? styles.dark : styles.light
-    ]);
+  renderTextItems = () => {
+    const themeStyle = this.props.isDark ? styles.dark : styles.light;
+    return TEXT_MAPS.map(({label, info}, index) => {
+      const labelStyles = classNames([
+        styles.label,
+        styles[`label${index}`],
+        themeStyle
+      ]);
+      const infoStyles = classNames([
+        styles.info,
+        styles[`info${index}`],
+        themeStyle
+      ]);
+      return ([
+        <span className={labelStyles}>{label}</span>,
+        <span className={infoStyles}>{info}</span>
+      ]);
+    });
+  }
+
+  renderTextGrid = () => {
     return (
-      <div className={textStyles}>
-        <p>
-          Hello! My name is Ben and I am a software engineer at Appian, currently working with React and React Native.
-          I have always been interested in visual design but have never had the chance to do such work on a daily basis.
-          In July 2019, I attended a web design conference, <a href="https://aneventapart.com/event/washington-dc-2019">An Event Apart DC</a>, which inspired me to create this site.
-          Here are a few things I hope to accomplish:
-        </p>
-        <ul>
-          <li>Learn more CSS and SVG</li>
-          <li>Design eye-catching and responsive UIs</li>
-          <li>Combine my technical and non-technical interests</li>
-        </ul>
-        <p>
-          Feel free to contact me on any of the platforms linked below. I hope you enjoy!
-        </p>
+      <div className={styles.grid}>
+        {this.renderTextItems()}
       </div>
-    
     );
   }
 
@@ -71,7 +79,7 @@ class About extends React.PureComponent {
     return (
       <div className={styles.container}>
         <img src={this.getBannerSrc()} className={styles.banner} alt="" />
-        {this.renderText()}
+        {this.renderTextGrid()}
       </div>
     );
   }
