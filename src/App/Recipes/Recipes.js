cb711f655981dc9cd41fb08bc4e87c6a3ad41a89
Ben Brott <ben.r.brott@gmail.com>
2020-03-19 01:11:44 -0400

    Basic functionality for recipes page

--- src/App/Recipes/Recipes.js
+++ src/App/Recipes/Recipes.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Recipes.module.scss';
 import { DATA } from './data';
 
 class Recipes extends React.PureComponent {
+  static propTypes = { isDark: PropTypes.bool }
+  static defaultProps = { isDark: false }
+
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
+  }
+
+  renderRecipe = ({ name, serves, prepTime, cookTime, ingredients }, isOpen) => {
+    const isDark = this.props.isDark;
+    const contents = isOpen ? ({}
+
+    ) : ({}
+
+    );
+
+    const themeClass = isDark ? styles.dark : styles.light;
+    return(
+      <div className={classNames([styles.container, themeClass])}>
+        <div className={classNames([styles.heading])}>
+          <h2 className={classNames([styles.name, themeClass])}>{name}</h2>
+          <span className={classNames([styles.info, themeClass])}>
+            Serves: {serves} | Prep: {prepTime} | Cook: {cookTime}
+          </span>
+        </div>
+        {/* {contents} */}
+      </div>
+    );
   }
 
   render() {
-    return (
-      <div/>
-    );
+    return DATA.map((recipe, index) => {
+      return this.renderRecipe(recipe, index === this.state.openIndex);
+    });
   }
 }
 
--- src/App/Recipes/data.js
+++ src/App/Recipes/data.js
@@ -1,2 +1,26 @@
+// Unicode fractions
+const U = {
+  '1/4': '\u00BC',
+  '1/2': '\u00BD',
+  '3/4': '\u00BE'
+};
+
 export const DATA = [
+  {
+    name: 'Patty\'s Fruit Cocktail Cake',
+    serves: 9,
+    prepTime: '15 min',
+    cookTime: '30 min',
+    ingredients: [
+      `${U['1/2']} cup unsalted butter, softened`,
+      `${U['1/4']} cup sugar`,
+      `1 large egg`,
+      `1 teaspoon baking soda`,
+      `${U['1/2']} teaspoon salt`,
+      `1${U['3/4']} cup flour`,
+      `1 16oz can fruit cocktail`,
+      `${U['1/2']} cup brown sugar`,
+      `${U['1/2']} cup chopped pecans`
+    ]
+  }
 ];
