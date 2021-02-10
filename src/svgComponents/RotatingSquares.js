3e26f439ce22368e55548bb55256013f52c3f430
Ben Brott <ben.r.brott@gmail.com>
2021-02-10 13:34:04 -0500

    Update crossword styling & fix loading animation

--- src/svgComponents/RotatingSquares.js
+++ src/svgComponents/RotatingSquares.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const RotatingSquares = ({ primaryColor }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" stroke={primaryColor} strokeWidth="3" strokeLinejoin="round" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" stroke={primaryColor} stroke-width="3" strokeLinejoin="round" fill="none">
       <path d="
         M 10,50       
         L 50,10       
@@ -12,7 +12,7 @@ const RotatingSquares = ({ primaryColor }) => {
         Z
       ">
         <animateTransform attributeName="transform" type="rotate" values="0 50 50;45 50 50;90 50 50;90 50 50"  dur="2s" repeatCount="indefinite"/>
-        <animate attributeName="strokeWidth" values="1;3;1;1;"  dur="2s" repeatCount="indefinite"/>
+        <animate attributeName="stroke-width" values="1;3;1;1;"  dur="2s" repeatCount="indefinite"/>
       </path>
       <path d="
         M 30,30       
@@ -22,7 +22,7 @@ const RotatingSquares = ({ primaryColor }) => {
         Z
       ">
         <animateTransform attributeName="transform" type="rotate" values="90 50 50;45 50 50;0 50 50;0 50 50"  dur="2s" repeatCount="indefinite"/>
-        <animate attributeName="strokeWidth" values="1;3;1;1;"  dur="2s" repeatCount="indefinite"/>
+        <animate attributeName="stroke-width" values="1;3;1;1;"  dur="2s" repeatCount="indefinite"/>
       </path>
       <path d="
         M 50,30       
@@ -32,7 +32,7 @@ const RotatingSquares = ({ primaryColor }) => {
         Z
       ">
         <animateTransform attributeName="transform" type="rotate" values="0 50 50;45 50 50;90 50 50;90 50 50"  dur="2s" repeatCount="indefinite"/>
-        <animate attributeName="strokeWidth" values="1;3;1;1;"  dur="2s" repeatCount="indefinite"/>
+        <animate attributeName="stroke-width" values="1;3;1;1;"  dur="2s" repeatCount="indefinite"/>
       </path>
     </svg>
   );
