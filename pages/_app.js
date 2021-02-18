7b69e5d0bb2d71315f76b4f878a4435714556116
Ben Brott <ben.r.brott@gmail.com>
2021-02-18 00:44:25 -0500

    Redesigned nav bar and home page

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,14 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import classNames from 'classnames';
 import 'styles/_global.scss';
 import styles from 'styles/_app.module.scss';
 import Food from 'svgComponents/Food';
 import CrosswordIcon from 'svgComponents/CrosswordIcon';
 import Headphones from 'svgComponents/Headphones';
-import House from 'svgComponents/House';
 import Loading from 'svgComponents/Loading';
 
 const NAV_ICONS = [
-  { Component: House, path: '/' },
   { Component: Headphones, path: '/music' },
   { Component: Food, path: '/recipes' },
   { Component: CrosswordIcon, path: '/crossword' },
@@ -25,17 +22,23 @@ const App = ({ Component, pageProps }) => {
   const renderNavBar = () => {
     const renderedIcons = NAV_ICONS.map(({ Component, path }, index) => {
       const currentIcon = path === router.pathname;
-      const classes = classNames([styles.iconContainer, currentIcon && styles.currentIcon]);
       const key = `navIcon_${index}`;
       return (
         <Link href={path} key={key}>
-          <a className={classes}>
-            <Component primaryColor={currentIcon ? '#000' : '#eee'} />
+          <a className={styles.icon}>
+            <Component primaryColor={'#eee'} />
           </a>
         </Link>
       );
     });
-    return <div className={styles.navBar}>{renderedIcons}</div>;
+    return (
+      <div className={styles.navBar}>
+        <Link href={'/'}>
+          <a className={styles.logoContainer} />
+        </Link>
+        <div className={styles.iconContainer}>{renderedIcons}</div>
+      </div>
+    );
   };
 
   return (
@@ -44,17 +47,15 @@ const App = ({ Component, pageProps }) => {
         <title>benbrott.com</title>
         <meta name="description" content="Ben Brott's React sandbox!"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="theme-color" content="#333" />
         <link rel="canonical" href="https://benbrott.com/" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <div className={styles.container}>
-        {renderNavBar()}
-        <div className={styles.pageContainer}>
-          <Component {...pageProps} />
-        </div>
+      {renderNavBar()}
+      <div className={styles.pageContainer}>
+        <Component {...pageProps} />
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,78 +1,57 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import Image from 'next/image';
 import styles from 'styles/index.module.scss';
-import withFormFactor from 'utils/withFormFactor';
 import GitHub from 'svgComponents/GitHub';
 import LinkedIn from 'svgComponents/LinkedIn';
 import Twitter from 'svgComponents/Twitter';
-import LogoNameLight from 'svgComponents/LogoNameLight';
-import LogoNameFlatLight from 'svgComponents/LogoNameFlatLight';
-
-const TEXT_MAPS = [
-  { label: 'WHO', info: 'Ben Brott, software engineer at Appian' },
-  { label: 'WHAT', info: 'A site inspired by talks at An Event Apart DC' },
-  { label: 'WHY', info: 'To learn CSS, explore SVG, and design eye-catching, responsive UIs' },
-  { label: 'HOW', info: 'React, Sass, Next.js, Netlify' }
-];
 
 const SOCIAL_LINKS = [
-  { Component: Twitter, href: 'https://twitter.com/BenBrott' },
   { Component: GitHub, href: 'https://github.com/BenBrott' },
-  { Component: LinkedIn, href: 'https://www.linkedin.com/in/brott/' }
+  { Component: LinkedIn, href: 'https://www.linkedin.com/in/brott/' },
+  { Component: Twitter, href: 'https://twitter.com/BenBrott' }
 ];
 
-const About = ({ isMobile }) => {
-  const getBannerImage = () => {
-    const image = isMobile ? <LogoNameLight /> : <LogoNameFlatLight />;
-    return <div className={styles.banner}>{image}</div>;
-  };
-
-  const renderTextGrid = () => {
-    const textItems = TEXT_MAPS.map(({ label, info }, index) => {
-      const labelStyles = classNames([styles.label, styles[`label${index}`]]);
-      const infoStyles = classNames([styles.info, styles[`info${index}`]]);
-      return [
-        <h4 key={`${label}_label`} className={labelStyles}>
-          {label}
-        </h4>,
-        <span key={`${label}_info`} className={infoStyles}>
-          {info}
-        </span>
-      ];
-    });
-    return (
-      <div key={'content'} className={styles.grid}>
-        {textItems}
-      </div>
-    );
-  };
-
-  const renderLinkItems = () => {
-    return SOCIAL_LINKS.map(({ Component, href }, index) => {
+const About = () => {
+  const renderSocialLinks = () => {
+    const links = SOCIAL_LINKS.map(({ Component, href }, index) => {
       const key = `social_link_${index}`;
       return (
-        <a key={key} className={styles.socialLink} href={href} target="_blank" rel="noopener noreferrer">
+        <a key={key} href={href} target="_blank" rel="noopener noreferrer">
           <Component />
         </a>
       );
     });
-  };
-
-  const renderSocialLinks = () => {
-    return <div className={styles.socialLinksContainer}>{renderLinkItems()}</div>;
+    return <div className={styles.linksContainer}>{links}</div>;
   };
 
   return (
     <div className={styles.container}>
-      {getBannerImage()}
-      {renderTextGrid()}
-      {renderSocialLinks()}
+      <div className={styles.textSection}>
+        <div className={styles.blurbRight}>
+          <h1>Ben Brott</h1>
+          <p>
+            I'm currently a front-end developer at Appian in the Washington D.C. area. Before that, I graduated from
+            Virginia Tech in 2018 with degrees in computer science and math. My interests include cooking, crocheting,
+            camping, video games, music, and sports.
+          </p>
+        </div>
+        <div className={styles.blurbLeft}>
+          <h1>benbrott.com</h1>
+          <p>
+            I was inspired to create this site after attending An Event Apart DC. The site is my sandbox to improve my
+            skills with traditional front-end technologies and modern interface design. All of the icons, save company
+            logos, are custom built SVGs. I use React, Next.js, Sass, and Netlify in my tech stack.
+          </p>
+        </div>
+      </div>
+      <div className={styles.photoSection}>
+        <div className={styles.imageContainer}>
+          <Image className={styles.image} src="/me.jpg" alt="Picture of Ben" layout="fill"></Image>
+        </div>
+        {renderSocialLinks()}
+      </div>
     </div>
   );
 };
 
-About.propTypes = { isMobile: PropTypes.bool };
-About.defaultProps = { isMobile: false };
-
-export default withFormFactor(About);
+export default About;
