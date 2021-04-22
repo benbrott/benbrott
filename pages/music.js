5cf09d5257754a2951f100e88d6319edbd64aea0
Ben Brott <ben.r.brott@gmail.com>
2021-04-22 18:06:14 -0400

    Use useCallback hook

--- pages/music.js
+++ pages/music.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import classNames from 'classnames';
 import styles from 'styles/music.module.scss';
 import DATA from 'data/music';
@@ -7,39 +7,44 @@ import KEY_EVENTS from 'utils/events';
 const Music = () => {
   const [openIndex, setOpenIndex] = useState();
 
-  const onAlbumClick = index => {
-    const newIndex = openIndex === index ? -1 : index;
-    setOpenIndex(newIndex);
-  };
+  const onAlbumClick = useCallback(
+    index => {
+      const newIndex = openIndex === index ? -1 : index;
+      setOpenIndex(newIndex);
+    },
+    [openIndex]
+  );
 
-  const onAlbumKeyDown = (event, index) => {
+  const onAlbumKeyDown = useCallback((event, index) => {
     if (KEY_EVENTS.ENTER(event)) {
       onAlbumClick(index);
     }
-  };
-
-  const renderedAlbums = DATA.map((album, index) => {
-    const { artist, title, src } = album;
-    const albumProps = {
-      className: classNames([styles.albumArt, openIndex === index && styles.open]),
-      onKeyDown: event => onAlbumKeyDown(event, index),
-      tabIndex: 0,
-      alt: `${title} by ${artist}`,
-      loading: 'lazy',
-      src
-    };
-    return (
-      <div className={styles.album} onClick={() => onAlbumClick(index)} key={`album_${index}`}>
-        <div className={styles.albumInfo}>
-          <span>{artist}</span>
-          <span>{title}</span>
-        </div>
-        <img {...albumProps} />
-      </div>
-    );
   });
 
-  return <div className={styles.grid}>{renderedAlbums}</div>;
+  return (
+    <div className={styles.grid}>
+      {DATA.map((album, index) => {
+        const { artist, title, src } = album;
+        const albumProps = {
+          className: classNames([styles.albumArt, openIndex === index && styles.open]),
+          onKeyDown: event => onAlbumKeyDown(event, index),
+          tabIndex: 0,
+          alt: `${title} by ${artist}`,
+          loading: 'lazy',
+          src
+        };
+        return (
+          <div className={styles.album} onClick={() => onAlbumClick(index)} key={`album_${index}`}>
+            <div className={styles.albumInfo}>
+              <span>{artist}</span>
+              <span>{title}</span>
+            </div>
+            <img {...albumProps} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Music;
